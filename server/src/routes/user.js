4fac54b952e458359c895d0126940be1369243e4
jilink <jimmyfac1@gmail.com>
2021-05-07 10:37:51 +0200

    crud in user

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -19,4 +19,25 @@ router.get('/:userId', async (req, res) => {
   return res.send(user);
 });
 
+router.post('/', async (req, res) => {
+  const user = await req.context.models.User.create(req.body);
+  return res.send(user);
+});
+
+router.put('/:userId', async (req, res) => {
+  const user = await req.context.models.User.findByPk(
+    req.params.userId,
+  );
+  user.update(req.body);
+  return res.send(user);
+});
+
+router.delete('/:userId', async (req, res) => {
+  const user = await req.context.models.User.findByPk(
+    req.params.userId,
+  );
+  user.destroy(req.body);
+  return res.send(user);
+});
+
 export default router;
