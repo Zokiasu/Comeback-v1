75ad13b225ce18df3fa667d1283d89e356b31d57
jilink <jimmyfac1@gmail.com>
2021-05-19 12:09:26 +0200

    fixing typo

--- server/src/routes/release.js
+++ server/src/routes/release.js
@@ -87,7 +87,7 @@ router.put('/:releaseId', async (req, res) => {
     req.body.newMusics,
     req.context.models.Music,
     (array) => release.addMusics(array),
-    (array) => release.createMusics(array),
+    (array) => release.createMusic(array),
   );
 
   return res.send(release);
