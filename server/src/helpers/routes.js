aa9b1bfbe8788fad11d5661831ab06b0a94ec675
jilink <jimmyfac1@gmail.com>
2021-05-19 08:25:07 +0200

    helper for associations when adding/creating an item

--- server/src/helpers/routes.js
+++ server/src/helpers/routes.js
@@ -8,3 +8,31 @@ export const queriesToDict = (queries) => {
 
   return { order: [order], limit, offset, where: queries };
 };
+
+export const addAssociationItems = async (
+  items,
+  newItems,
+  model,
+  addItems,
+  createItem,
+) => {
+  // setting exisiting
+  if (items) {
+    const ids = [];
+    for (const tmpItem of items) {
+      ids.push(tmpItem['id']);
+    }
+    const requestedItems = await model.findAll({
+      where: { id: ids },
+    });
+
+    addItems(requestedItems);
+  }
+
+  if (newItems) {
+    // creating new item and addig them
+    for (const newItem of newItems) {
+      createItem(newItem);
+    }
+  }
+};
