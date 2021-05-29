4888c3fa6ab240d99410e24954c04ba37bee98e5
jilink <jimmyfac1@gmail.com>
2021-05-29 19:10:40 +0200

    routes for users/id/notifications & users/id/artists©

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -65,6 +65,38 @@ router.get('/:userId', async (req, res) => {
   return res.send(user);
 });
 
+router.get('/:userId/artists', async (req, res) => {
+  const user = await req.context.models.User.findByPk(
+    req.params.userId,
+    {
+      include: [
+        {
+          model: req.context.models.Artist,
+          as: 'artists',
+          ...queriesToDict(req.query),
+        },
+      ],
+    },
+  );
+  return res.send(user?.artists || []);
+});
+
+router.get('/:userId/notifications', async (req, res) => {
+  const user = await req.context.models.User.findByPk(
+    req.params.userId,
+    {
+      include: [
+        {
+          model: req.context.models.Notification,
+          as: 'notifications',
+          ...queriesToDict(req.query),
+        },
+      ],
+    },
+  );
+  return res.send(user?.notifications || []);
+});
+
 router.post('/', async (req, res) => {
   const user = await req.context.models.User.create(req.body);
   return res.send(user);
