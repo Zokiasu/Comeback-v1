a3eb2ff52a2c5dc98df900da1a651df79ae732cc
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-06-01 17:05:56 +0200

    Add  loading

--- client/layouts/presentation.vue
+++ client/layouts/presentation.vue
@@ -1,26 +1,42 @@
 <template>
-  <div>
+  <div v-if="display">
     <div class="text-white flex space-x-3 justify-end m-5">
       <button @click="login=true" class="focus:outline-none px-3 py-1 rounded-sm flex justify-center transition duration-500 ease-in-out bg-gray-600 hover:bg-gray-500 transform hover:font-bold">Login</button>
       <button @click="signup=true" class="focus:outline-none px-3 py-1 rounded-sm flex justify-center transition duration-500 ease-in-out bg-red-900 hover:bg-red-700 transform hover:font-bold">Sign Up</button>
     </div>
-    <Modal v-model="login" title="Login">
+    <Modal 
+      v-model="login" 
+      title="Login"
+      wrapper-class="animate__animated"
+      :in-class="`animate__fadeInDown`"
+      :out-class="`animate__bounceOut`"
+      bg-class="animate__animated"
+      :bg-in-class="`animate__fadeInUp`"
+      :bg-out-class="`animate__fadeOutDown`">
       <div class="flex flex-col justify-center">
         <t-input type="email" v-model="auth.email" placeholder="Email" name="email" class="my-2"></t-input>
         <t-input type="password" v-model="auth.password" placeholder="Password" name="password" class="my-2"></t-input>
         <button @click="logIn()" class="focus:outline-none texts px-3 py-2 rounded-sm flex justify-center transition duration-500 ease-in-out bg-red-700 text-white hover:bg-red-900 transform hover:-translate-y-0.5 hover:scale-110 hover:font-bold my-2">Login</button>
       </div>
     </Modal>
-    <Modal v-model="signup" title="Signup">
+    <Modal 
+      v-model="signup" 
+      title="Signup"
+      wrapper-class="animate__animated"
+      :in-class="`animate__fadeInDown`"
+      :out-class="`animate__bounceOut`"
+      bg-class="animate__animated"
+      :bg-in-class="`animate__fadeInUp`"
+      :bg-out-class="`animate__fadeOutDown`">
       <div class="flex flex-col justify-center">
+        <t-input id="username" type="text" v-model="sign.username" placeholder="Username" name="username" class="my-2"></t-input>
         <t-input id="email" type="email" v-model="sign.email" placeholder="Email" name="email" class="my-2"></t-input>
         <t-input id="password" type="password" v-model="sign.password" placeholder="Password" name="password" class="my-2"></t-input>
-        <t-input id="username" type="text" v-model="sign.username" placeholder="Username" name="username" class="my-2"></t-input>
         <t-input id="confirm_password" type="password" v-model="sign.passwordCheck" placeholder="Confirm Password" name="confirm_password" class="my-2"></t-input>
         <button @click="signUp()" class="focus:outline-none texts px-3 py-2 rounded-sm flex justify-center transition duration-500 ease-in-out bg-red-700 text-white hover:bg-red-900 transform hover:-translate-y-0.5 hover:scale-110 hover:font-bold my-2">Sign Up</button>
       </div>
     </Modal>
-    <Nuxt />
+    <Nuxt class="animate__bounceIn" />
   </div>
 </template>
 
@@ -47,6 +63,7 @@
 
         passwordCheck: null,
         data: null,
+        display: false,
       }
     },
 
@@ -65,11 +82,23 @@
       })
     },
 
+    mounted() {
+      this.$nextTick(() => {
+        this.$nuxt.$loading.start()
+        setTimeout(() => this.displayPresentation(), 3300)
+      })
+    },
+
     methods: {
       ...mapActions([ // spread operator so that other methods can still be added.
         'updateToken',
         'updateUserId',
       ]),
+
+      displayPresentation(){
+        this.display = true
+        this.$nuxt.$loading.finish()
+      },
 
       async logIn(){
         let that = this
@@ -96,7 +125,43 @@
 
       async signUp(){
         await this.$axios.post('http://comeback-api.herokuapp.com/users/auth/signup', this.sign)
+        this.signup = false
+        this.$toast.success('You can login with your account')
       }
     },
   }
 </script>
+
+<style>
+  @import 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
+  .fullscreen-modal {
+    width: 100%;
+    max-width: 100%;
+    top: 0;
+    margin: 0;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .fullscreen-modal .vm-titlebar {
+    flex-shrink: 0;
+  }
+  .fullscreen-modal .vm-content {
+    padding: 0;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    min-height: 0; 
+  }
+  .fullscreen-modal .vm-content .scrollable-content {
+    position: relative;
+    overflow-y: auto;
+    overflow-x: hidden;
+    padding: 10px 15px 10px 15px;
+    flex-grow: 1;
+  }
+  .fullscreen-modal .fullscreen-modal-footer {
+    border-top: 1px solid #e5e5e5;
+    padding: 15px;
+  }
+</style>
--- client/pages/_userid/profile/index.vue
+++ client/pages/_userid/profile/index.vue
@@ -3,13 +3,13 @@
       <div id="top" class="flex mb-10 relative text-white" >
         <img class="h-80 w-full object-cover absolute border-b border-gray-500" :src="user.avatar ? user.avatar : require(`~/assets/image/profile.png`)" alt="Profile Picture"/>
 
-        <div id="profile-picture" class="z-10 flex flex-col justify-center mt-72 lg:mt-44 ml-5">
-            <img class="h-20 lg:h-48 w-20 lg:w-48 rounded-full object-cover border border-gray-500" :src="user.avatar ? user.avatar : require(`~/assets/image/profile.png`)" alt="Profile Picture"/>
-            <span class="font-semibold lg:text-2xl text-center">{{user.username}}</span>
+        <div id="profile-picture" class="z-10 flex flex-col justify-center mt-72 md:mt-44 ml-5">
+            <img class="h-20 md:h-40 w-20 md:w-40 rounded-full object-cover border border-gray-500" :src="user.avatar ? user.avatar : require(`~/assets/image/profile.png`)" alt="Profile Picture"/>
+            <span class="font-semibold md:text-2xl text-center">{{user.username}}</span>
         </div>
-        <div id="general" class="z-10 flex flex-col px-3 justify-end lg:pb-10">
+        <div id="general" class="z-10 flex flex-col px-3 justify-end md:pb-10">
             <nav>
-                <ul class="flex space-x-5 list-none text-lg lg:text-2xl">
+                <ul class="flex space-x-5 list-none text-lg md:text-2xl">
                     <li>
                         <div @click="changetab('artist')" class="cursor-pointer" :class="actualtab == 'artist' ? 'border-red-700 border-b-2' : ''">
                             <span>Artist</span>
@@ -33,7 +33,7 @@
         <div id="artist-following" v-if="actualtab == 'artist'">
             <h1 class="text-white text-xl">Artist Followed</h1>
             <div id="divider" class="border-b border-red-700 border-1 my-2 mb-2 w-96"></div>
-            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 gap-y-5 mt-10 my-5 w-full justify-center">
+            <div class="grid grid-cols-2 md:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 gap-y-5 mt-10 my-5 w-full justify-center">
                 <ArtistCard 
                     v-for="(artist, index) in user.artists"
                     :key="index"
@@ -44,7 +44,7 @@
         <div id="music-following" v-if="actualtab == 'music'">
             <h1 class="text-white text-xl">Releases Followed</h1>
             <div id="divider" class="border-b border-red-700 border-1 my-2 mb-2 w-96"></div>
-            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 gap-x-5 gap-y-10 gap-3 py-5 justify-center texts text-white">
+            <div class="grid grid-cols-2 md:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 gap-x-5 gap-y-10 gap-3 py-5 justify-center texts text-white">
                 <ReleaseCard
                     v-for="(release, index) in this.releaseList"
                     :width="true"
