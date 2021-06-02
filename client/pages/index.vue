6d6e7891cfb6d5cd9bab7f4d6f208bc916470507
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-06-02 04:21:11 +0200

    edit home page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -7,13 +7,16 @@
       <img class="object-cover" style="min-height:15rem;" src="https://firebasestorage.googleapis.com/v0/b/comeback-65643.appspot.com/o/images%2Flistartist.png?alt=media&token=ad9b2408-7966-4d03-bd50-4d41910ecc7f" alt="">
       <!--<img class="object-cover" style="min-height:15rem;" src="~/assets/image/listartist.jpg" alt="">-->
       <div class="absolute bg-mainbg bg-opacity-80 top-20 md:top-20 lg:top-10 2xl:top-16 ms:left-10">
-        <h2 class="text-white py-5 px-10 md:text-xl lg:text-2xl xl:text-3xl w-full" style="max-width:45rem;line-height: 2em;">Find your favorite artists and track all their comeback in one place</h2>
+        <h2 class="text-white py-5 px-10 md:text-xl lg:text-2xl xl:text-3xl w-full" style="max-width:45rem;line-height: 2em;">Find your favorite artists and track all their single, album and EP in one place</h2>
       </div>
     </div>
     <div class="flex justify-center text-white my-32">
       <span class="text-center text-xl md:text-3xl xl:text-4xl mx-10 2xl:mx-60">Create with us the best place where you can find and track all your favorite musicians.</span>
     </div>
-    <div class="w-full mt-20 px-10">
+    <div class="w-full px-10">
+      <div class="w-full flex flex-col justify-center border-t-2 border-gray-500 py-5">
+        <span class="w-full text-center text-white">This website is currently under development, so you may encounter some bugs while using it.</span>
+      </div>
       <div class="w-full flex flex-col space-y-5 justify-center border-t-2 border-gray-500 py-5">
         <ul class="flex space-x-1.5 text-white justify-center text-xl">
           <li>
@@ -63,8 +66,6 @@
             <span>Cozy Codeur</span>
           </li>
         </ul>
-        
-        <span class="w-full text-center text-white pt-5">This website is currently under development, so you may encounter some bugs while using it.</span>
       </div>
     </div>
   </div>
