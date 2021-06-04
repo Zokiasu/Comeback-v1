39127e7eee46317b91aff0b7d66516b4bf71f7ba
jilink <jimmyfac1@gmail.com>
2021-06-04 20:05:03 +0200

    migrations for last commit

---
+++ server/migrations/20210604175712-releases-to-release-in-music.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // music belongsTo release
+    return queryInterface
+      .addColumn(
+        'musics', // name of Source model
+        'releaseId', // name of the key we're adding
+        {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'releases', // name of Target model
+            key: 'id', // key in Target model that we're referencing
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+        },
+      )
+      .then(() => {
+        // Release hasMany Music
+        return queryInterface.addColumn(
+          'musics', // name of Target model
+          'releaseId', // name of the key we're adding
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'musics', // name of Source model
+              key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+          },
+        );
+      });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Music_Release');
+  },
+};
