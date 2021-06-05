6131c5da42b083a53361875db9caa57febe3d67d
jilink <jimmyfac1@gmail.com>
2021-06-05 09:21:07 +0200

    migrations

--- server/migrations/20210604175712-releases-to-release-in-music.js
+++ server/migrations/20210604175712-releases-to-release-in-music.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // music belongsTo release
-    return queryInterface
+    /* return queryInterface
       .addColumn(
         'music', // name of Source model
         'releaseId', // name of the key we're adding
@@ -32,10 +32,10 @@ module.exports = {
             onDelete: 'SET NULL',
           },
         );
-      });
+      });*/
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Music_Release');
+    /*return queryInterface.dropTable('Music_Release'),*/
   },
 };
