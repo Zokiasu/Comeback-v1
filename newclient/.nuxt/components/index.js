69bb9376d0a00bdc220324c6e61622016153915b
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-07-02 12:17:44 +0200

    test

---
+++ newclient/.nuxt/components/index.js
@@ -0,0 +1,11 @@
+import { wrapFunctional } from './utils'
+
+export { default as ArtistCard } from '../..\\components\\ArtistCard.vue'
+export { default as Logo } from '../..\\components\\Logo.vue'
+export { default as Menu } from '../..\\components\\Menu.vue'
+export { default as ReleaseCard } from '../..\\components\\ReleaseCard.vue'
+
+export const LazyArtistCard = import('../..\\components\\ArtistCard.vue' /* webpackChunkName: "components/artist-card" */).then(c => wrapFunctional(c.default || c))
+export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
+export const LazyMenu = import('../..\\components\\Menu.vue' /* webpackChunkName: "components/menu" */).then(c => wrapFunctional(c.default || c))
+export const LazyReleaseCard = import('../..\\components\\ReleaseCard.vue' /* webpackChunkName: "components/release-card" */).then(c => wrapFunctional(c.default || c))
