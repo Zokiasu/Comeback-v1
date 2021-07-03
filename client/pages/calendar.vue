5e67295befef1f60783bef9cc8ef1355a6c5b5d5
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-07-03 06:57:11 +0200

    Update calendar.vue

--- client/pages/calendar.vue
+++ client/pages/calendar.vue
@@ -92,7 +92,7 @@
     },
 
     async asyncData({ $axios }){
-        const newsList = await $axios.$get('https://comeback-api.herokuapp.com/infos?sortby=createdAt:desc&limit=5')
+        const newsList = await $axios.$get('https://comeback-api.herokuapp.com/infos?sortby=createdAt:desc&limit=10')
         return {newsList}
     },
 
