d89f66121d5d693270fa427fefb2eaed8d4aa611
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-08-08 09:58:42 +0200

    optimize loading searchbar artist

--- client/pages/artist/index.vue
+++ client/pages/artist/index.vue
@@ -4,13 +4,13 @@
       <div id="search-icon" class="bg-opacity-20 bg-gray-500 pr-1 pl-2 rounded-none rounded-l py-1.5">
         <svg class="" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 172 172" style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M64.5,14.33333c-27.6214,0 -50.16667,22.54527 -50.16667,50.16667c0,27.6214 22.54527,50.16667 50.16667,50.16667c12.52732,0 23.97256,-4.67249 32.7819,-12.31771l3.05143,3.05143v9.26628l43,43l14.33333,-14.33333l-43,-43h-9.26628l-3.05143,-3.05143c7.64521,-8.80934 12.31771,-20.25458 12.31771,-32.7819c0,-27.6214 -22.54527,-50.16667 -50.16667,-50.16667zM64.5,28.66667c19.87509,0 35.83333,15.95824 35.83333,35.83333c0,19.87509 -15.95825,35.83333 -35.83333,35.83333c-19.87509,0 -35.83333,-15.95825 -35.83333,-35.83333c0,-19.87509 15.95824,-35.83333 35.83333,-35.83333z"></path></g></g></svg>
       </div>
-      <input @change="updateDateList(true)" id="search-input" type="text" placeholder="Search" v-model="search" class="w-full pl-2 focus:outline-none rounded-r rounded-none bg-opacity-20 bg-gray-500 text-white placeholder-white">
+      <input id="search-input" type="text" placeholder="Search" v-model="search" class="w-full pl-2 focus:outline-none rounded-r rounded-none bg-opacity-20 bg-gray-500 text-white placeholder-white">
     </section>
-    <section v-if="filteredList.length > 0" id="artist-list" class="grid grid-cols-2 ms:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 1xl:grid-cols-7 2xl:grid-cols-10 gap-5 w-full justify-center p-5">
-      <ArtistCard class="animate__animated animate__fadeIn" v-for="artist in filteredList" :key="artist.id" :artist="artist"/>
+    <section v-if="artists.length > 0" id="artist-list" class="grid grid-cols-2 ms:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 1xl:grid-cols-7 2xl:grid-cols-10 gap-5 w-full justify-center p-5">
+      <ArtistCard class="animate__animated animate__fadeIn" v-for="artist in artists" :key="artist.id" :artist="artist"/>
     </section>
-    <InfiniteLoading spinner="spiral" @infinite="infiniteScroll"></InfiniteLoading>
-    <div v-if="filteredList.length < 1" class="px-5">
+    <InfiniteLoading v-if="!enough" :identifier="infiniteId" spinner="spiral" @infinite="infiniteScroll"></InfiniteLoading>
+    <div v-if="artists.length < 1" class="px-5">
       <span style="background-color: #6B728033" class="text-white w-full flex justify-center rounded p-2">No artists found.</span>
     </div>
   </div>
@@ -34,12 +34,14 @@
         artists: [],
         maxArtist: 0,
         enough: false,
+        resetSearch: false,
+        infiniteId: +new Date(),
       }
     },
         
     computed: {
       filteredList() {
-        return this.artists
+        return this.updateDateList(this.resetSearch)
       },
 
       widthActual(){
@@ -51,21 +53,33 @@
       },
     },
 
+    watch: {
+        search: {
+            immediate: true,
+            handler(search) {
+              if(search != '') {
+                this.resetSearch = true
+                this.updateDateList(true)
+              }
+              console.log('resetSearch', this.resetSearch)
+            }
+        }
+    },
+
     methods:{
-      async fetchData() {
-        let artTmp = []
-        console.log("Search", `https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`)
-        const {data: response} = await this.$axios.get(`https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`);
-        artTmp = artTmp.concat(response)
-        this.artists = [...new Set(artTmp)]
+      changeType() {
+        this.infiniteId += 1;
       },
+
       infiniteScroll($state) {
         let artTmp = []
+        this.enough = false
         setTimeout(() => {
           artTmp = artTmp.concat(this.artists)
-          console.log("Search", `https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`)
+          console.log(`https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`)
           this.$axios.get(`https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`).then(response => {
-            if(response.data.length > 0) {
+            console.log('response.data.length',response.data.length)
+            if(response.data.length) {
               artTmp = artTmp.concat(response.data)
               this.artists = [...new Set(artTmp)]
               this.maxArtist = this.maxArtist + 20
@@ -83,9 +97,10 @@
 
       async updateDateList(reset) {
         let artTmp = []
+        this.enough = false
         if(reset) {
           this.maxArtist = 0
-          console.log("Search", `https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`)
+          console.log(`https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`)
           const {data: response} = await this.$axios.get(`https://comeback-api.herokuapp.com/artists/groups?sortby=name&name=%${this.search}%&op=ilike&limit=20&offset=${this.maxArtist}`)
           if(response.length > 0) {
             artTmp = artTmp.concat(response)
@@ -97,8 +112,11 @@
             }
           } else {
             this.enough = true
+            this.artists = []
           }
+          this.resetSearch = false
         }
+        this.changeType()
       },
     },
   }
