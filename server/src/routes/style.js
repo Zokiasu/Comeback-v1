0bfadcf8a2c8747bdea75aa1f5a67c36c73c92f4
jilink <jimmyfac1@gmail.com>
2021-08-15 10:36:14 +0200

    add releases and artists routes for style

--- server/src/routes/style.js
+++ server/src/routes/style.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { queriesToDict } from '../helpers/routes';
+import { queriesToDict, sortArrayBy } from '../helpers/routes';
 
 const router = Router();
 
@@ -45,6 +45,40 @@ router.get('/:styleId', async (req, res) => {
     },
   );
   return res.send(style);
+});
+
+router.get('/:styleId/artists', async (req, res) => {
+  const style = await req.context.models.Style.findByPk(
+    req.params.styleId,
+    {
+      include: [req.context.models.Artist],
+    },
+  );
+  const artists = style.artists;
+  // we need this because we can't sort by with classic queries
+  const sortBy = req.query.sortby?.split(':') || [
+    'createdAt',
+    'DESC',
+  ];
+  sortArrayBy(artists, sortBy);
+  return res.send(artists);
+});
+
+router.get('/:styleId/releases', async (req, res) => {
+  const style = await req.context.models.Style.findByPk(
+    req.params.styleId,
+    {
+      include: [req.context.models.Release],
+    },
+  );
+  const releases = style.releases;
+  // we need this because we can't sort by with classic queries
+  const sortBy = req.query.sortby?.split(':') || [
+    'createdAt',
+    'DESC',
+  ];
+  sortArrayBy(releases, sortBy);
+  return res.send(releases);
 });
 
 router.post('/', async (req, res) => {
