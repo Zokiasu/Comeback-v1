ece0cda366257e09f19a8fa0ab49fd4bbdf0b45b
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-08-17 14:50:39 +0200

    Update NewsCard.vue

--- client/components/NewsCard.vue
+++ client/components/NewsCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <NuxtLink :to="`/artist/${element.artist.id}`" class="flex flex-col space-y-5 bg-gray-500 bg-opacity-30 text-white Card rounded-xl overflow-clip w-56">
+    <NuxtLink :to="`/artist/${element.artist.id}`" class="Card flex flex-col space-y-5 bg-gray-500 bg-opacity-30 text-white rounded-xl w-56">
         <div class="flex justify-between px-3 py-2">
             <div class="flex flex-col">
                 <div class="font-semibold">
