c2078ba14b3b2f7cbec792820a71370e69cd2c53
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-08-21 22:50:11 +0200

    Update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="px-10 py-5 pb-16 overflow-hidden space-y-10">
     <section id="newAnnounce" class="section">
       <div class="flex space-x-5">
-        <h2 class="text-xl sm:text-2xl lg:text-4xl text-white py-5 flex">Coming Soon<NuxtLink :to="`/news`" class="ml-2 mt-auto text-sm focus:outline-none">View More</NuxtLink></h2>
+        <h2 class="text-xl sm:text-2xl lg:text-4xl text-white py-5 flex">Coming Soon</h2>
       </div>
       <transition-group name="object" class="flex flex-wrap w-full justify-center lg:justify-start inner">
         <NewsCard class="Card news my-1.5 md:m-2" v-for="(element) in newsList" :key="element.id" :element="element"/>
