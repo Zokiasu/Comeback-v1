d0573ca5fa48b9b55c801e03ad6f4ae623f6b59a
Pierrick Taïly <32719751+Zokiasu@users.noreply.github.com>
2021-10-09 10:05:37 +0200

    Update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-10 py-5 pb-16 overflow-hidden space-y-10">
-    <section id="newAnnounce" class="section">
+    <section id="newAnnounce" class="section" v-if="newsList.length > 1">
       <div class="flex w-full justify-start space-x-5">
         <h2 class="text-xl sm:text-2xl lg:text-4xl text-white py-5 flex">Next Comeback</h2>
       </div>
@@ -8,7 +8,7 @@
         <NewsCard class="Card news my-1.5 md:m-2" v-for="(element) in newsList" :key="element.id" :element="element"/>
       </transition-group>
     </section>
-    <section id="newRelease" class="section">
+    <section id="newRelease" class="section" v-if="newRelease.length > 1">
       <div>
         <h2 class="text-xl sm:text-2xl lg:text-4xl text-white py-5 flex">Last Release Added<NuxtLink :to="`/release`" class="ml-2 mt-auto text-sm  focus:outline-none">View More</NuxtLink></h2>
       </div>
@@ -16,7 +16,7 @@
         <ReleaseCard class="release md:mr-5 md:mb-5 justify-self-center" v-for="(release) in newRelease" :key="release.id" :release="release"/>
       </transition-group>
     </section>
-    <section id="newArtist" class="section">
+    <section id="newArtist" class="section" v-if="newArtist.length > 1">
       <div>
         <h2 class="text-xl sm:text-2xl lg:text-4xl text-white py-5 flex">Last Artist Added<NuxtLink :to="`/artist`" class="ml-2 mt-auto text-sm  focus:outline-none">View More</NuxtLink></h2>
       </div>
