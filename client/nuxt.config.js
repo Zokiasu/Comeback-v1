05f26a7f075fee559506c83e0cc0771dd218f4b5
Zokiasu <pierrick.tly@gmail.com>
2022-03-06 02:41:08 +0100

    test nuxt lazyload

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -82,7 +82,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    'nuxt-lazy-load'
   ],
 
   eslint: {
